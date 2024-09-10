d25ca39f5cc189f547133a4f84997ccfb14fcfa9
SumitReactProjects <reactproject18@gmail.com>
2024-09-10 13:01:39 +0530

    Google Auth Part two (client work completed)

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -3,7 +3,12 @@ import { AiFillGoogleCircle } from "react-icons/ai";
 import { signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userslice";
+import { useNavigate } from "react-router-dom";
 const OAuth = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleGoogleClick = async () => {
     const auth = getAuth(app);
 
@@ -13,7 +18,22 @@ const OAuth = () => {
 
     try {
       const resultFromGoogleProvider = await signInWithPopup(auth, provider);
-      console.log(resultFromGoogleProvider);
+      //   console.log(resultFromGoogleProvider);
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: resultFromGoogleProvider.user.displayName,
+          email: resultFromGoogleProvider.user.email,
+          googlePhotoUrl: resultFromGoogleProvider.user.googlePhotoUrl,
+        }),
+      });
+
+      const data = await res.json();
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
