fb191e1de86083450ab7262241a94a310d3600b1
SumitReactProjects <reactproject18@gmail.com>
2024-09-18 10:01:34 +0530

    image Upload Functionality (part-2 hide the file input using ref hook)

--- client/src/Components/Dashprofile.jsx
+++ client/src/Components/Dashprofile.jsx
@@ -1,17 +1,22 @@
 import { Button, TextInput } from "flowbite-react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useSelector } from "react-redux";
 
 const Dashprofile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  // use Ref
+  const filePickerRef = useRef();
   // setting image file and URL
   const [imageFile, setImageFile] = useState(null);
   const [imageFileURL, setImageFileURL] = useState(null);
   const handleImageChange = async (e) => {
+    // Selecting file from input Feilds
     const file = e.target.files[0];
-
+    // Check the file exist or not if exist
     if (file) {
+      // set image
       setImageFile(file);
+      // and create its URL
       setImageFileURL(URL.createObjectURL(file));
     }
   };
@@ -20,8 +25,17 @@ const Dashprofile = () => {
     <div className="mx-auto max-w-lg w-full p-3">
       <h1 className="text-center text-3xl font-semibold my-7">Profile</h1>
       <form className="flex flex-col gap-6">
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <div className="w-32 h-32 self-center mx-auto cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          ref={filePickerRef}
+          onChange={handleImageChange}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center mx-auto cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
             src={imageFileURL || currentUser.profilePhoto}
             alt="User"
