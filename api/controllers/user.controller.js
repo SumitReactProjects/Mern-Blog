158d83b1f6477ddf5f46576c298fbb43e01b3399
SumitReactProjects <reactproject18@gmail.com>
2024-09-20 17:05:45 +0530

    Add update user Route (Part 2 adding user update functionality)

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,61 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.send({ message: "Api Connected" });
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log(req.user);
+  // console.log(req.user);
+
+  if (req.user.id !== req.params.userId) {
+    next(errorHandler(403, "You are not allowed to update user"));
+  }
+
+  // Password Feild
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must atleast 6 Characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must between 6 to 20 Characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username does not contain space"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username has lower case Characters"));
+    }
+    if (req.body.username.match(/^a-zA-Z+$/)) {
+      return next(
+        errorHandler(400, "Username only Contains numbers and Letters")
+      );
+    }
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePhoto: req.body.profilePhoto,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
