9064dd00e1c437d2f5634eb2b213f86abfd99288
SumitReactProjects <reactproject18@gmail.com>
2024-11-02 17:56:20 +0530

    add preinstallation code

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoute from "../api/routes/auth.route.js";
 import postRoute from "../api/routes/post.route.js";
 import commentRoute from "../api/routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,6 +37,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoute);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
